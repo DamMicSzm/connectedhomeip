f8d8fbfc19944b6b05332b3ba644a73b2f9eb80c
Damian Michalak-Szmaciński <d.michalak@samsung.com>
2023-07-26 12:01:39 +0000

    Fix uninitialised value in KeySet

--- src/credentials/GroupDataProvider.h
+++ src/credentials/GroupDataProvider.h
@@ -121,10 +121,10 @@ public:
     {
         static constexpr size_t kLengthBytes = Crypto::CHIP_CRYPTO_SYMMETRIC_KEY_LENGTH_BYTES;
         // Validity start time in microseconds since 2000-01-01T00:00:00 UTC ("the Epoch")
-        uint64_t start_time;
+        uint64_t start_time = 0;
         // Actual key bits. Depending on context, it may be a raw epoch key (as seen within `SetKeySet` calls)
         // or it may be the derived operational group key (as seen in any other usage).
-        uint8_t key[kLengthBytes];
+        uint8_t key[kLengthBytes] = {};
 
         void Clear()
         {
@@ -202,7 +202,7 @@ public:
     virtual ~GroupDataProvider() = default;
 
     // Not copyable
-    GroupDataProvider(const GroupDataProvider &) = delete;
+    GroupDataProvider(const GroupDataProvider &)             = delete;
     GroupDataProvider & operator=(const GroupDataProvider &) = delete;
 
     uint16_t GetMaxGroupsPerFabric() const { return mMaxGroupsPerFabric; }
